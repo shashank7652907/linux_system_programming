f9031673a1ec44223cc56e1da84d19894e991e18
Shashank Adepu <shashankadepu@gmail.com>
2026-02-04 11:03:17 +0530

    Update codw with better example

--- thread_mechanisms/argumentThread.c
+++ thread_mechanisms/argumentThread.c
@@ -7,31 +7,39 @@
 int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
 void* routine(void* arg){
-    sleep(1);
-    int index = *((int *)arg);
-    printf("[%d]",index);
-    free(arg);
-    pthread_exit(0);
+    int sum = 0;
+    int a = *(int *)arg;
+    for(int i = a; i < a + 5; i ++){
+        sum += primes[i];
+    }
+    *(int *)arg = sum;
+    printf("Local sum : %d\n",sum);
+    return arg;
 }
 
 int main(int argc, char* argv[]){
     pthread_t th[10];
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 2; i++){
         int* a = (int *)malloc(sizeof(int));
-        *a = i;
+        *a = i * 5;
         if(pthread_create(&th[i], NULL, &routine, a) == -1){
             printf("Thread create failed [%d]",i);
             return i + 1;
         }
         printf("Thread [%d] created\n",i);
     }
-
-    for(int i = 0; i < 10;i++){
-        if(pthread_join(th[i], NULL) == -1){
+    int globalSum = 0;
+    for(int i = 0; i < 2;i++){
+        int* r;
+        if(pthread_join(th[i], (void **)&r) == -1){
             printf("Thread [%d] join failed\n",i);
             return i + 7;
         }
+        globalSum +=  *r;
+        free(r);
     }
+    printf("Sum is %d ",globalSum);
+    
     printf("\n");
 
     return 0;
